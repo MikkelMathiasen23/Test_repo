bbef11defb8940d0cdbc0867f35f23fac64a46b0
MikkelMathiasen23 <51484172+MikkelMathiasen23@users.noreply.github.com>
2020-10-14 15:59:17 +0200

    commit message

---
+++ src/forecast_p/pipelines/data_processing/pipeline.py
@@ -0,0 +1,11 @@
+from kedro.pipeline import node, Pipeline
+from forecast_p.pipelines.data_processing.nodes import (
+
+)
+
+def create_pipeline(**kwargs):
+    return Pipeline(
+        [
+            
+        ]
+    )
